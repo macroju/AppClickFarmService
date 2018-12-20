83c76ac9add951199368f0114df20c7219ee054a
hongguan.ju <hongguan.ju@dxc.com>
2018-12-20 13:22:29 +0800

    create project

---
+++ src/test/java/com/clickfarm/demo/AppClickFarmServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.clickfarm.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppClickFarmServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
